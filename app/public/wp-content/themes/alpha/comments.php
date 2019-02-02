8fd494f3d540e3d29c42288dc3500e5e855485a1
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-02-03 00:18:52 +0600

    class-8.3
    ৮.৩ - wp_list_comments ব্যবহার করে কমেন্টের লিস্ট দেখানো এবং পেজিনেশন

--- app/public/wp-content/themes/alpha/comments.php
+++ app/public/wp-content/themes/alpha/comments.php
@@ -1,47 +1,45 @@
-<?php
-//Get only the approved comments 
-$args = array(
-    'status' => 'approve',
-    'post_id' => get_the_ID(  ),
-);
- 
-// The comment Query
-$comments_query = new WP_Comment_Query;
-$comments = $comments_query->query( $args );
- 
-?>
-<div class="comment"><!-- comment start -->
-    <?php
-    // Comment Loop
-    if ( $comments ) {
-        foreach ( $comments as $comment ) {
-            ?>
-                <div class="media"><!-- media start -->
 
-                    <?php echo get_avatar( $comment, 64, null, null, array(
-                        'class' => 'mr-3',
-                    )  ) ;?>
-
-                    <div class="media-body">
-                        <h5 class="mt-0">
-                            <?php comment_author($comment);?>
-                            <?php edit_comment_link();?>
-                        </h5>
-                        <?php
-                            comment_text( $comment );
-                        ?>
-                    </div>
-                </div><!-- media end -->
-
-            <?php
-        }
-    } else {
-        echo 'No comments found.';
-    }
-    ?>
-    <div class="comment-form">
+<div class="comments">
+    <h2 class="comments-title">
         <?php
-            comment_form();
+        $alpha_cn= get_comments_number();
+        if(1 == $alpha_cn){
+            _e("1 Comment","alpha");
+        }else {
+            echo $alpha_cn . " " . __("Comments","alpha");
+        }
+        
+        ?>
+    </h2>
+
+    <div class="comments-list">
+        <?php
+        wp_list_comments(  );
+
+        ?>
+
+        <div class="comments-pagination">
+            <?php
+                the_comments_pagination(array(
+                    'screen_reader_text' => __('Pagination', 'alpha'),
+                    'prev_text' => '<' . __('Previous comments', 'alpha'),
+                    'next_text' => '>' . __('Next comments', 'alpha'),                    
+                ));
+            ?>
+        </div>
+        <?php
+
+        if(!comments_open()){
+            _e("Comments are closed.","alpha");
+        }
         ?>
     </div>
-</div><!-- comment end -->
+
+    
+
+    <div class="comments-form">
+        <?php
+        comment_form(  );
+        ?>
+    </div>
+</div><!-- comments end -->
